bfe0e6f2ea961f49886a63b082b92db159e19a52
pw0908 <pjwalker@caltech.edu>
2024-08-19 15:49:40 -0700

    Update PMF code

---
+++ run_pmf.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=a0.0_LBOUND
+#SBATCH --time=2-00:00:00
+# Slurm: Node configuration
+#SBATCH --partition=subset
+#SBATCH --account=zgw
+#SBATCH --qos=zgw
+
+#SBATCH --nodes=1 --ntasks-per-node=2 --mem=4G
+#SBATCH --gres=gpu:0 --gpu-bind=closest
+
+#SBATCH --output=/home/pwalker/slurm-reports/slurm-%j.out --error=/home/pwalker/slurm-reports/slurm_error-%j.out
+
+# -*- coding: utf-8 -*-
+#
+# Author     : Kelsey Fu 
+# Date       : 2024-06-25
+# Description: Script to modify the LAMMPS input file and run the simulation
+
+# Define the current working directory
+CWD_PATH=$(pwd)
+LAMMPS_PATH="/home/kfu/Polyampholyte-SURF/software/lammps/build/lmp"
+
+
+# source "script/simulate.sh" # 1 & 100 chain
+source "script/simulate_pmf.sh"
+# source "script/simulate_bulk.sh"
+# source "script/simulate_pmf  Only 2 chain
+# # source "script/analysis.sh" Only cluster analysis 4 100 chain
+
+
+
+
+
+
---
+++ submit_pmf.sh
@@ -0,0 +1,16 @@
+source "inputs/0.0chi.sh"
+
+# Iterate over lower bounds 0 to 25 in increments of 5
+for LBOUND in $(seq 0 5 5)
+do
+    cp run_pmf.sh run_pmf_$LBOUND.sh
+    sed -i "s/LBOUND/$LBOUND/g" run_pmf_$LBOUND.sh
+ 
+
+    # Define the upper bound
+    export UBOUND=$(($LBOUND + 5))
+    export LBOUND
+    # Run the simulation
+    sbatch run_pmf_$LBOUND.sh
+    rm run_pmf_$LBOUND.sh
+done
