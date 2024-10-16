2c9a5ef8137bf7a1a2afe1ea7209a0e8375384c0
kelseyfu <kelseyfu@g.ucla.edu>
2024-10-16 11:19:30 -0700

    update repository

---
+++ inputs/double_chain_0.0_0_1.0.sh
@@ -0,0 +1,15 @@
+export SEQUNAME="0.0ALPHA"
+export sequence="[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]"
+export ZNET="0"
+
+export TEMP="1"
+export NCHAIN="2"
+export NSALT="2000"
+export LENGTHX="50"
+export LENGTHY="50"
+export LENGTHZ="50"
+
+export STEPS="100000000"
+
+export NCPU="2"
+export NGPU="0"
