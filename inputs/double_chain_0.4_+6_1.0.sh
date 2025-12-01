9968635247720b80b78ca4c13be09254c5f736bb
pw0908 <pw1017@ic.ac.uk>
2025-12-01 15:36:00 -0800

    Update input files

---
+++ inputs/double_chain_0.4_+6_1.0.sh
@@ -0,0 +1,15 @@
+export SEQUNAME="0.4ALPHA"
+export sequence="[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]"
+export ZNET="6"
+
+export TEMP="1"
+export NCHAIN="2"
+export NSALT="2000"
+export LENGTHX="50"
+export LENGTHY="50"
+export LENGTHZ="50"
+
+export STEPS="100000000"
+
+export NCPU="4"
+export NGPU="0"
